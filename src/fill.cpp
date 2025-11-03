8ab7d1a4f7b2488a8028b0a5ee745c973f47962f
Vadym Grabovenko <playstation4love@gmail.com>
2025-11-03 12:49:54 +0200

    Singleton OOP task

--- src/fill.cpp
+++ src/fill.cpp
@@ -5,5 +5,15 @@
 
 void fill_data()
 {
-    // IMPLEMENT: FILL DATA HERE
+    auto& uc = a1::Singleton<a1::UserConfig>::getInstance();
+    auto& sc = a1::Singleton<a1::SystemConfig>::getInstance();
+    uc.add_user("Hordiyenko","Z-32");
+    uc.add_user("Hrabovenko","Z-32");
+    uc.add_user("Zamjatin","Z-32");
+
+    sc.add_system("Hordiyenko", "MacOS");
+    sc.add_system("Hrabovenko", "Linux");
+    sc.add_system("Zamjatin", "Windows");
+
+
 }
