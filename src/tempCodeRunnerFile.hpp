4a1aceb27ceddebef08181ee7e5de9b76d753921
Artem <artembykanov332@gmail.com>
2025-11-04 23:13:38 +0300

    Final working version

---
+++ src/tempCodeRunnerFile.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace a1 {
+
+template <typename T>
+class Singleton
+{
+public:
+    static T& getInstance()
+    {
+        static T instance;
+
+        return instance;
+    }
+
+private:
+    Singleton() = default;
+    ~Singleton() = default;
+};
+
+}
