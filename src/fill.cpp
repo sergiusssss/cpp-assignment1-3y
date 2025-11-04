beef197f397979b6d10233b3eaa9d55e3c5c8dad
Razvalinov Vova <sociophob.don@gmail.com>
2025-11-04 20:33:18 +0200

    Зроблено

--- src/fill.cpp
+++ src/fill.cpp
@@ -9,16 +9,19 @@ using a1::SystemConfig;
 
 void fill_data()
 {
-    
+
     UserConfig& user_conf = Singleton<UserConfig>::getInstance();
     SystemConfig& system_conf = Singleton<SystemConfig>::getInstance();
 
-    user_conf.add_user("Povoroznjuk", "Z-32");
-    system_conf.add_system("Povoroznjuk", "MacOS");
-
-    user_conf.add_user("Razvalinov", "Z-32");
-    system_conf.add_system("Razvalinov", "Windows");
-
+    // 1. Додаємо попередню людину (Рєпін)
     user_conf.add_user("Ryepin", "Z-32");
     system_conf.add_system("Ryepin", "Windows");
+
+    // 2. Додаємо себе (Тонкова)
+    user_conf.add_user("Tonkova", "Z-32");
+    system_conf.add_system("Tonkova", "MacOS");
+
+    // 3. Додаємо наступну людину (Чабан)
+    user_conf.add_user("Chaban", "Z-32");
+    system_conf.add_system("Chaban", "Linux");
 }
