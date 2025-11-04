24b65befc866cc00d7f470c8a3e01cac157ff738
Daniil Povoroznuk <lokydeep@gmail.com>
2025-11-04 19:08:53 +0200

    DICH

--- src/fill.cpp
+++ src/fill.cpp
@@ -5,5 +5,15 @@
 
 void fill_data()
 {
-    // IMPLEMENT: FILL DATA HERE
+    auto& uc = a1::Singleton<a1::UserConfig>::getInstance();
+    auto& sc = a1::Singleton<a1::SystemConfig>::getInstance();
+    uc.add_user("Nakonechnyj","Z-32");
+    uc.add_user("Povoroznjuk","Z-32");
+    uc.add_user("Razvalinov","Z-32");
+
+    sc.add_system("Nakonechnyj", "Linux");
+    sc.add_system("Povoroznjuk", "MacOS");
+    sc.add_system("Razvalinov", "Windows");
+
+
 }
