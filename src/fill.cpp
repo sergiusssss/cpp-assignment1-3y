4f4cc35efcd76de1526cbeef852cbe10e58a1df5
Vladislav Marchenko <vlaislav2016@gmail.com>
2025-11-04 14:26:24 +0200

    сохранение

--- src/fill.cpp
+++ src/fill.cpp
@@ -2,29 +2,38 @@
 
 #include <user_config.hpp>
 #include <system_config.hpp>
+#include <vector>
+#include <string>
 
 namespace a1 {
-    void fill_data()
-    {
-        auto& userCfg = Singleton<UserConfig>::getInstance();
-        auto& sysCfg = Singleton<SystemConfig>::getInstance();
 
-        std::vector<std::pair<std::string, std::string>> users = {
-            {"Marchenko", "Z-31"},
-            {"Dvornyk", "Z-31"},
-            {"Nazarenko", "Z-31"}
-        };
+void fill_data_impl()
+{
+    auto& userCfg = Singleton<UserConfig>::getInstance();
+    auto& sysCfg = Singleton<SystemConfig>::getInstance();
 
-        std::vector<std::pair<std::string, std::string>> systems = {
-            {"Marchenko", "Windows"},
-            {"Dvornyk", "MacOS"},
-            {"Nazarenko", "Windows"}
-        };
+    std::vector<std::pair<std::string, std::string>> users = {
+        {"Lavrov", "Z-31"},
+        {"Marchenko", "Z-31"},
+        {"Merkova", "Z-31"}
+    };
 
-        for (auto& u : users)
-            userCfg.add_user(u.first, u.second);
+    std::vector<std::pair<std::string, std::string>> systems = {
+        {"Lavrov", "Linux"},
+        {"Marchenko", "Windows"},
+        {"Merkova", "Windows"}
+    };
 
-        for (auto& s : systems)
-            sysCfg.add_system(s.first, s.second);
-    }
+    for (auto& u : users)
+        userCfg.add_user(u.first, u.second);
+
+    for (auto& s : systems)
+        sysCfg.add_system(s.first, s.second);
+}
+
+}
+
+void fill_data()
+{
+    a1::fill_data_impl();
 }
