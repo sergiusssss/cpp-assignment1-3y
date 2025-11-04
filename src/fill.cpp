323e9334e517b2051eb2baf12f0a805648e5948f
Razvalinov Vova <sociophob.don@gmail.com>
2025-11-04 20:23:49 +0200

    Сделано

--- src/fill.cpp
+++ src/fill.cpp
@@ -3,7 +3,22 @@
 #include <user_config.hpp>
 #include <system_config.hpp>
 
+using a1::Singleton;
+using a1::UserConfig;
+using a1::SystemConfig;
+
 void fill_data()
 {
-    // IMPLEMENT: FILL DATA HERE
+    
+    UserConfig& user_conf = Singleton<UserConfig>::getInstance();
+    SystemConfig& system_conf = Singleton<SystemConfig>::getInstance();
+
+    user_conf.add_user("Povoroznjuk", "Z-32");
+    system_conf.add_system("Povoroznjuk", "MacOS");
+
+    user_conf.add_user("Razvalinov", "Z-32");
+    system_conf.add_system("Razvalinov", "Windows");
+
+    user_conf.add_user("Ryepin", "Z-32");
+    system_conf.add_system("Ryepin", "Windows");
 }
