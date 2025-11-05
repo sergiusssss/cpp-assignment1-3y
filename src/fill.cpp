3f2feb15f8227344c84652fb91aa9be5acb5f71b
Artem1049 <artembykanov332@gmail.com>
2025-11-05 14:57:33 +0300

    Add fill_data implementation for users and systems
    Implement data filling for users and systems.

--- src/fill.cpp
+++ src/fill.cpp
@@ -2,8 +2,38 @@
 
 #include <user_config.hpp>
 #include <system_config.hpp>
+#include <vector>
+#include <string>
+
+namespace a1 {
+
+void fill_data_impl()
+{
+    auto& userCfg = Singleton<UserConfig>::getInstance();
+    auto& sysCfg = Singleton<SystemConfig>::getInstance();
+
+    std::vector<std::pair<std::string, std::string>> users = {
+        {"Lavrov", "Z-31"},
+        {"Marchenko", "Z-31"},
+        {"Merkova", "Z-31"}
+    };
+
+    std::vector<std::pair<std::string, std::string>> systems = {
+        {"Lavrov", "Linux"},
+        {"Marchenko", "Windows"},
+        {"Merkova", "Windows"}
+    };
+
+    for (auto& u : users)
+        userCfg.add_user(u.first, u.second);
+
+    for (auto& s : systems)
+        sysCfg.add_system(s.first, s.second);
+}
+
+}
 
 void fill_data()
 {
-    // IMPLEMENT: FILL DATA HERE
+    a1::fill_data_impl();
 }
