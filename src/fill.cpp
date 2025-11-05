cb34813f418128e71e9b0ca96859595e5b16dbb8
Andrii Shumko <andriishumko@gmail.com>
2025-11-05 12:03:19 +0200

    Done 3

--- src/fill.cpp
+++ src/fill.cpp
@@ -8,14 +8,13 @@ void fill_data()
     auto& uc = a1::Singleton<a1::UserConfig>::getInstance();
     auto& sc = a1::Singleton<a1::SystemConfig>::getInstance();
 
-    uc.add_user("Akimov","Z-32");
-    sc.add_system("Akimov", "MacOS");
+    uc.add_user("Shukajlo", "Z-32");
+    sc.add_system("Shukajlo", "Windows");
 
     uc.add_user("Shumko","Z-32");
     sc.add_system("Shumko", "Windows");
 
     uc.add_user("Al'terovych","Z-32");
     sc.add_system("Al'terovych", "MacOS");
-
 
 }
