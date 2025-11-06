855232a07309baaa78ea40cc3924429e32968768
Ilia Arsenev <arsenevila8@gmail.com>
2025-11-06 12:50:39 +0200

    Implement singleton, user/system configs, and fill_data

--- src/fill.cpp
+++ src/fill.cpp
@@ -5,5 +5,14 @@
 
 void fill_data()
 {
-    // IMPLEMENT: FILL DATA HERE
+    auto& users  = a1::Singleton<a1::UserConfig>::getInstance();
+    auto& systems = a1::Singleton<a1::SystemConfig>::getInstance();
+
+    users.add_user("Shvets'",   "Z-31");
+    users.add_user("Arsen'yev", "Z-31");
+    users.add_user("Bykanov",   "Z-31");
+
+    systems.add_system("Shvets'",   "MacOS");
+    systems.add_system("Arsen'yev", "Linux");
+    systems.add_system("Bykanov",   "MacOS");
 }
